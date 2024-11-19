6a7b9bda627c7552938b4cdae007315591948a93
Aidan Hildebrand <blackbirdgt3rs@gmail.com>
2024-11-18 18:28:46 -0800

    adds countdown timer

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Code your solutions in this file
+//const names = ["Aly", "Aidan", "Rabbi"];
+
+//function writeCards(names) {
+    //for (let i = 0; i < names.length; i++ ) {
+        //console.log(`Thank you, ${names[i]}, for the wonderful birthday gift!`);
+        
+    //}
+   //return names;
+//}
+
+//writeCards(names);
+///////////////////////////////////////
+//function writeCards(names) {
+    //let i = 0; 
+    //while (i < names.length) {
+        //console.log(`Thank you, ${names[i]}, for the wonderful birthday gift!`);
+        //i++
+    //}
+   //return names;
+//}
+//writeCards(names)
+/////////////////////////////////////
+
+const time = 10
+function countDown(time) {
+   // let time = 10
+    while (time >= 0) {
+        console.log(time)
+        time--;
+    }
+    return time
+}
+countDown(time)
